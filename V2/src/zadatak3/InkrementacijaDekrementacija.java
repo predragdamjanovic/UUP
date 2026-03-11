63a302b4adc11a05ccfd0ff633d3d88e8e5d15d8
Predrag Damjanovic <predragdamjanovic16@gmail.com>
2026-03-11 17:06:49 +0100

    Izmena V2, izmena zadatka3

--- V2/src/zadatak3/InkrementacijaDekrementacija.java
+++ V2/src/zadatak3/InkrementacijaDekrementacija.java
@@ -18,5 +18,26 @@ public class InkrementacijaDekrementacija {
         --y;
         System.out.println("Sada y iznosi: " + y);
 
+        //pre i post inkrementacija
+        int a = 3;
+        int b = 2;
+        int c = 1;
+
+        System.out.println("Broj b iznosi: "+b);
+        // post inkrementacija
+        int d = a + b++;
+        System.out.println("Vrednost broja d je: "+d);
+        /* izlaz na ekranu je broj 5, razlog tome je jer su se prvo
+        sabrali a i b pa b uvecao za 1
+        */
+        //Sada stampamo vrednost pod b
+        System.out.println("Nova vrednost broja b je: "+b);
+        // preinkrementacija
+        int e = a + ++c;
+        /*
+        Sada je izlaz na ekranu 5 ,jer se prvo izvrsila inkrementacija pa su
+        se brojevi sabrali
+         */
+        System.out.println("Vrednost broja e je : "+e);
     }
 }
