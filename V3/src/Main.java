80b65ce74a9251431709dcd350f36e258e7188da
Predrag Damjanovic <predragdamjanovic16@gmail.com>
2026-03-12 23:00:38 +0100

    Doradjena vezba v3

--- V3/src/Main.java
+++ V3/src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Unesite neki broj: ");
+        int unetiBroj = sc.nextInt();
+
+        if (unetiBroj > 500) {
+            System.out.println("Uneti broj je veci od 500");
+        } else {
+            System.out.println("Uneti broj je manji od 500");
         }
     }
+}
